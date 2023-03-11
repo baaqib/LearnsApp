e7c066d40716722b8ea6b8a98520145004636d05
sajidhassan1 <sajidhasan15@gmail.com>
2023-03-11 12:01:33 +0530

    fix issue

--- src/Components/Institute.js
+++ src/Components/Institute.js
@@ -2,32 +2,30 @@ import React, { useState } from "react";
 import apiClient from "./http-common";
 
 const Institute = () => {
-    const [result, setResult] = useState([]);
-    async function getInstitute() {
-        try {
-            const res = await apiClient.get("/Institute/GetInstitute");
-            console.warn(res.data);
-            setResult(result => [...result, res.data]);
-        } catch (err) {
-            console.warn(err);
-        }
+  const [result, setResult] = useState([]);
+  async function getInstitute() {
+    try {
+      const res = await apiClient.get("/Institute/GetInstitute");
+      console.warn(res.data);
+      setResult(res.data);
+    } catch (err) {
+      console.warn(err);
     }
-    return (
-        <>
-            <h1>Institute:</h1>
-            {/* <h2>{a.id}</h2> */}
-            <div className="App">
-      <ul>
-
-        {result.map((data) => (
-
-        <li key={data.id}>{data.name}</li>
-        ))}
-      </ul>
-    </div>
-            <button onClick={getInstitute}>getit</button>
-        </>
-    );
-}
+  }
+  return (
+    <>
+      <h1>Institute:</h1>
+      {/* <h2>{a.id}</h2> */}
+      <div className="App">
+        <ul>
+          {result.map((data) => (
+            <li key={data.id}>{data.name}</li>
+          ))}
+        </ul>
+      </div>
+      <button onClick={getInstitute}>getit</button>
+    </>
+  );
+};
 
 export default Institute;
