def7b553a048f375cf5a6ef0799daf1a73a60d83
baaqib <baaqi2@gmail.com>
2023-03-13 12:56:48 +0530

    getbyid

--- src/Components/Institute.js
+++ src/Components/Institute.js
@@ -3,19 +3,34 @@ import apiClient from "./http-common";
 
 const Institute = () => {
   const [result, setResult] = useState([]);
+  const [resul, setResul] = useState({id:0,name:''});
+
   async function getInstitute() {
     try {
       const res = await apiClient.get("/Institute/GetInstitute");
-      console.warn(res.data);
       setResult(res.data);
     } catch (err) {
       console.warn(err);
     }
   }
+
+  async function getInstituteByID() {
+    // const id = get_id.current.value;
+    const id = 2;
+    if (id) {
+      try {
+        const re = await apiClient.get(`/Institute/GetInstituteByID/${id}`);
+        console.warn(re.data);
+        setResul(re.data);
+      } catch (err) {
+        console.warn(err);
+      }
+    }
+  }
+
   return (
     <>
       <h1>Institute:</h1>
-      {/* <h2>{a.id}</h2> */}
       <div className="App">
         <ul>
           {result.map((data) => (
@@ -23,7 +38,9 @@ const Institute = () => {
           ))}
         </ul>
       </div>
+      <h1>{resul.name}</h1>
       <button onClick={getInstitute}>getit</button>
+      <button onClick={getInstituteByID}>getInstituteByID</button>
     </>
   );
 };
